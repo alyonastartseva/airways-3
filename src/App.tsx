66c4b9808a9b68a311e23255eced489e75b34809
Isa <abazovkjks@gmail.com>
2023-01-10 14:34:27 +0600

    (rebase) main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ChakraProvider } from "@chakra-ui/react";
 import "@/App.css";
 import { additional, departure, Footer, ret } from "@components/Footer";
 
-const queryClient = new QueryClient();
-
 function App() {
-  return (
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <Footer
-          departure={departure}
-          return={ret}
-          // additional={additional}
-        />
-      </QueryClientProvider>
-    </ChakraProvider>
-  );
+  return <Footer departure={departure} return={ret} additional={additional} />;
 }
 
 export default App;
