b6f166157a1bc17d6469207c7e4b9ff0aa203aeb
Isa <abazovkjks@gmail.com>
2023-01-12 13:00:42 +0600

    font, logo image

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Footer } from "@components/Footer";
 import { QueryClient, QueryClientProvider } from "react-query";
+import theme from "@utils/theme";
+import "@fontsource/open-sans/700.css";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <App />
