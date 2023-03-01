2e56fae85c9a59927a3067ee97306086a00e82cd
abhorrentgauda <trainxcrypt0x@gmail.com>
2023-03-01 19:10:40 +0300

    fix login/improve destintaion tests

--- src/components/Admin/Destinations/Destinations.test.tsx
+++ src/components/Admin/Destinations/Destinations.test.tsx
@@ -1,32 +1,54 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, vi } from 'vitest';
+import { afterEach, describe, expect, vi } from 'vitest';
 
 import Destinations from './Destinations';
 
+vi.mock('react-query');
+
 describe('Destinations', () => {
-  it('Destinations render table data', () => {
-    vi.mock('react-query', async () => {
-      const testData = {
-        data: [
-          {
-            id: 1,
-            airportCode: 'VVO',
-            airportName: 'Кневичи',
-            countryName: 'Россия',
-            timezone: 'GMT +10',
-          },
-        ],
-      };
-      return {
-        useQuery: vi.fn().mockReturnValue({ data: testData }),
-        useMutation: vi.fn().mockReturnValue({}),
-      };
-    });
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  it('Destinations render table data', async () => {
+    const testData = {
+      data: [
+        {
+          id: 1,
+          airportCode: 'VVO',
+          airportName: 'Кневичи',
+          countryName: 'Россия',
+          timezone: 'GMT +10',
+        },
+      ],
+    };
+    const data = await import('react-query');
+    data.useQuery = vi.fn().mockReturnValue({ data: testData });
+    data.useMutation = vi.fn().mockReturnValue({});
 
     render(<Destinations />);
     expect(screen.getByText('VVO')).toBeInTheDocument();
     expect(screen.getByText('Кневичи')).toBeInTheDocument();
     expect(screen.getByText('Россия')).toBeInTheDocument();
     expect(screen.getByText('GMT +10')).toBeInTheDocument();
+  });
+
+  it('Destinations render spinner', async () => {
+    const data = await import('react-query');
+    data.useQuery = vi.fn().mockReturnValue({ isLoading: true });
+    data.useMutation = vi.fn().mockReturnValue({});
+
+    render(<Destinations />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('Destinations render alert', async () => {
+    const data = await import('react-query');
+    data.useQuery = vi.fn().mockReturnValue({});
+    data.useMutation = vi.fn().mockReturnValue({});
+
+    render(<Destinations />);
+    expect(screen.getByText('Something went wrong')).toBeInTheDocument();
   });
 });
