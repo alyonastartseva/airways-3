17368f33f0d72c10a1a2c0f3ad2d59d68849f7db
abhorrentgauda <trainxcrypt0x@gmail.com>
2023-03-02 19:51:27 +0300

    refactor: header button route to home

--- src/common/icons/index.ts
+++ src/common/icons/index.ts
@@ -7,3 +7,5 @@ export { default as PenIcon } from './Pen';
 export { default as PlaneTabIcon } from './PlaneTabIcon';
 export { default as ResetIcon } from './Reset';
 export { default as Logo } from './Logo';
+export { default as AppStore } from './AppStore';
+export { default as PlayStore } from './PlayStore';
