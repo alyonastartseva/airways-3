f02d612d0729b85741d5429ff8c0a6b7dfac08f2
abhorrentgauda <trainxcrypt0x@gmail.com>
2023-03-02 19:01:51 +0300

    DELETE: getHeaders function, so login is required

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -5,39 +5,36 @@ import {
   IDestinationPost,
 } from '@interfaces/search.interfaces';
 import { IAirplane, IAirplanePost } from '@interfaces/plane.interfaces';
-import { ILoginRequest } from '@interfaces/login.interfaces';
+// import { ILoginRequest } from '@interfaces/login.interfaces';
 import ERoutes from '@services/endpoints';
 
 class searchService {
-  admin: ILoginRequest = {
-    password: 'admin',
-    username: 'admin@mail.ru',
-  };
+  // admin: ILoginRequest = {
+  //   password: 'admin',
+  //   username: 'admin@mail.ru',
+  // };
 
-  getHeaders = async () => {
-    let token;
-    if (localStorage.getItem('token')) {
-      token = localStorage.getItem('token');
-    } else {
-      const res = await axiosInstance.post(ERoutes.LOGIN_ADMIN, {
-        ...this.admin,
-      });
-      localStorage.setItem('token', res.data.accessToken as string);
-      token = res.data.accessToken;
-    }
-    return {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    };
-  };
+  // getHeaders = async () => {
+  //   let token;
+  //   if (localStorage.getItem('token')) {
+  //     token = localStorage.getItem('token');
+  //   } else {
+  //     const res = await axiosInstance.post(ERoutes.LOGIN_ADMIN, {
+  //       ...this.admin,
+  //     });
+  //     localStorage.setItem('token', res.data.accessToken as string);
+  //     token = res.data.accessToken;
+  //   }
+  //   return {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //   };
+  // };
 
   getDestinations = async () => {
-    return await axiosInstance.get<IDestination[]>(
-      ERoutes.DESTINATION,
-      await this.getHeaders()
-    );
+    return await axiosInstance.get<IDestination[]>(ERoutes.DESTINATION);
   };
 
   patchDestinations = async (data: IDestination | null) => {
@@ -45,75 +42,52 @@ class searchService {
       const { id, ...rest } = data;
       return await axiosInstance.patch<IDestination>(
         ERoutes.DESTINATION + id,
-        rest,
-        await this.getHeaders()
+        rest
       );
     }
   };
 
   deleteDestination = async (id: number | undefined) => {
     if (id) {
-      return await axiosInstance.delete<IDestination>(
-        ERoutes.DESTINATION + id,
-        await this.getHeaders()
-      );
+      return await axiosInstance.delete<IDestination>(ERoutes.DESTINATION + id);
     }
   };
 
   postDestinations = async (data: IDestinationPost) => {
     return await axiosInstance.post<IDestinationPost>(
       ERoutes.DESTINATION,
-      data,
-      await this.getHeaders()
+      data
     );
   };
 
   getAircrafts = async () => {
-    return await axiosInstance.get<IAirplane[]>(
-      ERoutes.AIRCRAFT,
-      await this.getHeaders()
-    );
+    return await axiosInstance.get<IAirplane[]>(ERoutes.AIRCRAFT);
   };
 
   postAircraft = async (data: IAirplanePost) => {
-    return await axiosInstance.post<IAirplanePost>(
-      ERoutes.AIRCRAFT,
-      data,
-      await this.getHeaders()
-    );
+    return await axiosInstance.post<IAirplanePost>(ERoutes.AIRCRAFT, data);
   };
 
   deleteAircraft = async (id: number | undefined) => {
     if (id) {
-      return await axiosInstance.delete<IAirplane>(
-        ERoutes.AIRCRAFT + id,
-        await this.getHeaders()
-      );
+      return await axiosInstance.delete<IAirplane>(ERoutes.AIRCRAFT + id);
     }
   };
 
   patchAircraft = async (data: IAirplane | null) => {
     if (data) {
       const { id, ...rest } = data;
-      return await axiosInstance.patch<IAirplane>(
-        ERoutes.AIRCRAFT + id,
-        rest,
-        await this.getHeaders()
-      );
+      return await axiosInstance.patch<IAirplane>(ERoutes.AIRCRAFT + id, rest);
     }
   };
 
   getSearchId = async (searchQuery: ISearchQuery) => {
-    return axiosInstance.post<number>(
-      '/search',
-      { ...searchQuery },
-      await this.getHeaders()
-    );
+    return axiosInstance.post<number>('/search', { ...searchQuery });
   };
 
   getSearchResult = async (searchQuery: ISearchQuery) => {
     const { data } = await this.getSearchId(searchQuery);
-    return axiosInstance.get(`/search/${data}`), await this.getHeaders();
+    return axiosInstance.get(`/search/${data}`);
   };
 }
 
