5714b19208436e7479c21c2bbf5a3647958102ad
abhorrentgauda <trainxcrypt0x@gmail.com>
2023-03-02 19:07:38 +0300

    REFACTOR: optimizing images

--- src/common/icons/index.ts
+++ src/common/icons/index.ts
@@ -6,3 +6,4 @@ export { default as ChevronIcon } from './Chevron';
 export { default as PenIcon } from './Pen';
 export { default as PlaneTabIcon } from './PlaneTabIcon';
 export { default as ResetIcon } from './Reset';
+export { default as Logo } from './Logo';
