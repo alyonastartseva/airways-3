eb00d6641ca77c12c5c55c114141db502a1a334f
MichailMakovchuk <krasnodar2312@mail.ru>
2023-03-27 07:47:01 +0000

    rename: dir Form => InputField, file Input => InputField; create: file...

---
+++ src/common/InputField/InputField.test.tsx
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { describe, vi } from 'vitest';
+
+import InputField from './InputField';
+
+describe('InputField test', () => {
+  vi.mock('react-hook-form', () => ({
+    useFormContext: () => ({
+      formState: () => vi.fn,
+      register: () => vi.fn,
+    }),
+  }));
+  beforeEach(() => {
+    render(<InputField name={'name'} label={'name'} />);
+  });
+  it('Render label text', () => {
+    const labelText = screen.getByText('name');
+    expect(labelText).toBeInTheDocument();
+  });
+  it('Render input element', () => {
+    const inputElement = screen.getByRole('textbox');
+    expect(inputElement).toBeInTheDocument();
+  });
+  it('Input element is empty', () => {
+    const inputElement = screen.getByLabelText('name');
+    expect(inputElement).toBeEmpty();
+  });
+});
