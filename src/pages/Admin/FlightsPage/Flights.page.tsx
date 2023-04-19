46ff65f80dc5e64c0af835adab98391a98e4e519
Nadezhda <nadia.vysokoos@gmail.com>
2023-04-19 14:43:45 +0000

    Flights page

--- src/pages/Admin/FlightsPage/Flights.page.tsx
+++ src/pages/Admin/FlightsPage/Flights.page.tsx
@@ -1,6 +1,12 @@
+import { Navigate } from 'react-router-dom';
+
 import { Flights } from '@/components/Admin/Flights';
+import ELinks from '@/services/adminRouterLinks.service';
 
 function FlightsPage() {
+  if (!localStorage.getItem('adminToken')) {
+    return <Navigate to={ELinks.ADMIN_LOGIN} />;
+  }
   return <Flights />;
 }
 
