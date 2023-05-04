7bdaff13efbcefd329876f3f51b6a98f9d891fc4
Polina Yakunina <yakunina1607@yandex.ru>
2023-05-04 20:57:55 +0300

    bugfix: add link from not found to main page

--- src/common/PageNotFound/PageNotFound.tsx
+++ src/common/PageNotFound/PageNotFound.tsx
@@ -1,12 +1,23 @@
-import { Flex, Alert, AlertIcon, AlertTitle } from '@chakra-ui/react';
+import { Flex, Alert, AlertIcon, AlertTitle, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-const PageNotFound = () => (
-  <Flex minHeight="81vh" justifyContent="center" alignItems="center">
-    <Alert status="error" justifyContent="center" width="20rem" height="6rem">
-      <AlertIcon />
-      <AlertTitle>PAGE NOT FOUND</AlertTitle>
-    </Alert>
-  </Flex>
-);
+const PageNotFound = () => {
+  const isAdmin = Boolean(localStorage.getItem('adminToken'));
+  const pathToMainPage: string = isAdmin ? '/passengers' : '/';
+
+  return (
+    <Flex minHeight="81vh" justifyContent="center" alignItems="center">
+      <Alert status="error" justifyContent="center" width="20rem" height="6rem">
+        <AlertIcon />
+        <AlertTitle>PAGE NOT FOUND</AlertTitle>
+        <Link to={pathToMainPage}>
+          <Button mr="1em" justifySelf="flex-end" fontSize="lx">
+            На главную
+          </Button>
+        </Link>
+      </Alert>
+    </Flex>
+  );
+};
 
 export default PageNotFound;
