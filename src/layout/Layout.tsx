c0cc45554cc8d357bcd8b593af1c5c17f67abcbd
masha.x <m_khvatova99@mail.ru>
2024-01-27 16:56:19 +0700

    (fix) fix footer position

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import { Flex } from '@chakra-ui/react';
 
 import { Footer } from '@components/Footer';
 import { Header } from '@/components/Header';
 
 const Layout = () => {
   return (
-    <>
+    <Flex
+      flexDirection={'column'}
+      justifyContent={'space-between'}
+      overflow={'hidden'}
+      minHeight={'100vh'}
+    >
       {<Header />}
       <Outlet />
       {<Footer />}
-    </>
+    </Flex>
   );
 };
 export default Layout;
