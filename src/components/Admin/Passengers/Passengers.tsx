5531545d7df483b314067754266ba8aad664d9c0
Alexandr Lisovoy <sashaliss18@gmail.com>
2024-05-25 22:36:48 +0500

    fix: accidentally deleted

--- src/components/Admin/Passengers/Passengers.tsx
+++ src/components/Admin/Passengers/Passengers.tsx
@@ -50,6 +50,16 @@ const Passengers = () => {
   const passengers = dataQuery?.content;
   const totalPages = dataQuery?.totalPages;
 
+  // если удален последняя строка текущей страницы, то открываем предыдущую страницу
+  useEffect(() => {
+    if (!passengers && pageIndex > 0) setPaginationData(pageIndex - 1);
+  }, [passengers, pageIndex, setPaginationData]);
+
+  useEffect(() => {
+    const currPage = Number(localStorage.getItem('PASSENGERS_CURR_PAGE'));
+    if (currPage > 0) setPaginationData(currPage);
+  }, [setPaginationData]);
+
   // стейт и индекс изменяемой строки
   const [editableRowIndex, setEditableRowIndex] = useState<number | null>(null);
   const [editableRowState, setEditableRowState] = useState<IPassenger | null>(
