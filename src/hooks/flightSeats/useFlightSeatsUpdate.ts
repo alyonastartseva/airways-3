faed0c420a5f6f9fd55304faa144f45b9b7841ed
ComRety <333Tretiakov@gmail.com>
2024-06-15 20:40:17 +0300

    fix: one

--- src/hooks/flightSeats/useFlightSeatsUpdate.ts
+++ src/hooks/flightSeats/useFlightSeatsUpdate.ts
@@ -4,14 +4,13 @@ import { updateFlightSeats } from '@/services/flightSeats/flightSeats.service';
 
 import { useRequestErrorToast } from '../useRequestErrorToast';
 
-
 export const useFlightSeatsPatch = () => {
-    const queryClient = useQueryClient();
-    const errorHandler = useRequestErrorToast();
-    return useMutation(updateFlightSeats, {
-        onSuccess: () => {
-            queryClient.invalidateQueries('flight-seats');
-        },
-        onError: errorHandler,
-    });
+  const queryClient = useQueryClient();
+  const errorHandler = useRequestErrorToast();
+  return useMutation(updateFlightSeats, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('flight-seats');
+    },
+    onError: errorHandler,
+  });
 };
