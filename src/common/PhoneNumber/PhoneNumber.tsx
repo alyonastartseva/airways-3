419ef8fc0f2f10f4a2fc6374acc62d42ba43b272
lartwoods <lartwoods@gmail.com>
2024-08-07 01:03:25 +0300

    style: fix header

--- src/common/PhoneNumber/PhoneNumber.tsx
+++ src/common/PhoneNumber/PhoneNumber.tsx
@@ -1,4 +1,5 @@
 import { Box, Text, Link } from '@chakra-ui/react';
+import './phonenumber.scss';
 
 const PhoneNumber = () => {
   return (
@@ -8,11 +9,13 @@ const PhoneNumber = () => {
       alignItems="center"
       height="40px"
       data-testid="phoneNumber"
+      className="phoneNumber"
     >
       <Text
         color="white"
         lineHeight="1.125rem"
         fontWeight="semibold"
+        width="120px"
         _hover={{ cursor: 'default' }}
       >
         <Link href="tel:+88007855896">+8 800 785 58 96</Link>
